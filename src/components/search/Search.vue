1e06543807744d3fa8dbff3bc210bd27782433a9
Heitor Guimarães <hguimaraes@poli.ufrj.br>
2016-07-28 00:20:02 -0300

    Improving search method

--- src/components/search/Search.vue
+++ src/components/search/Search.vue
@@ -44,16 +44,37 @@
       <div class="row">
         <div class="input-field col s8">
           <i class="material-icons prefix">search</i>
-          <input id="search_email" type="tel" class="validate">
+          <input id="search_email" type="email" v-model="search_email">
           <label for="search_email">Email</label>
         </div>
         <div class="input-field col s4">
-          <a class="waves-effect waves-light btn orange">Search</a>
+          <a class="waves-effect waves-light btn orange" v-on:click="searchUser">Search</a>
         </div>
       </div>
     </form>
   </div>
-  <br><br>  
+  <br><br>
+  <table class="striped center" v-show="showResult">
+        <thead>
+          <tr>
+              <th data-field="id">Name</th>
+              <th data-field="name">Email</th>
+              <th data-field="price">Add</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td width="50%">{{searched_user.name}}</td>
+            <td width="40%">{{searched_user.email}}</td>
+            <td width="10%">
+              <a class="btn-floating btn waves-effect waves-light blue center">
+                <i class="material-icons">add</i>
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
 </div>
 </template>
 
@@ -73,7 +94,7 @@ export default {
         "email" : "",
         "name" : ""
       },
-
+      search_email: "",
       showResult: false
 		};
 	},
@@ -83,22 +104,19 @@ export default {
    */
   methods: {
     searchUser : function(event) {
-      this.$http.post(API_ADDRESS + 'search/' 
-          + this.signin.email + '/').then((response) => {
+      this.$http.get(API_ADDRESS + 'search/' 
+          + this.search_email + '/').then((response) => {
             // 200 RESPONSE
             if(response.data.success){
               // Show the found user 
               this.searched_user.email = response.data.user.email;
               this.searched_user.name = response.data.user.n;
-              router.go('/home');
-
+              this.showResult = true;
             } else {
               // Throw an error in the HTML element
               Materialize.toast("User not found, please check the email again", 3000, 'rounded');
+              this.showResult = false;
             }
-            
-            // Reset the signin form
-            this.resetSignIn();
 
           }, (response) => {
           // Redirect to error message/page
